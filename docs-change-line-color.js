292f51310600a974902c84232525b09bca12402c
sunakan <sunakan03@gmail.com>
2022-08-03 09:35:41 +0900

    特定の文字列から始まる行の文字色を変更するスニペット追加

---
+++ docs-change-line-color.js
@@ -0,0 +1,16 @@
+/**
+ * 特定の文字列から始まる行の文字色を変更する
+ * 
+ * startWord: 特定の文字色
+ * foregroundColor: 変更したい文字色(RGB)
+ */
+function myFunction() {
+  let startWord = "> ";
+  let foregroundColor = "#b7b7b7";
+  DocumentApp.getActiveDocument()
+    .getBody()                                                 // Body
+    .getParagraphs()                                           // Array<Paragraph>
+    .map(paragraph => paragraph.editAsText())                  // Array<Text>
+    .filter(text => text.getText().startsWith(startWord))      // Array<Text>
+    .forEach(text => text.setForegroundColor(foregroundColor)) // 色を上書き
+}
